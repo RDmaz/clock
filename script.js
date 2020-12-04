1829f5ab7d1912115c6563ecc21c205cb34eab2b
Rami David <56394661+RDmaz@users.noreply.github.com>
2020-12-04 21:12:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const currentDate = new Date;
+    const secondRatio = currentDate.getSeconds() / 60;
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+    const hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+    setRoation(hourHand,hourRatio);
+    setRoation(secondHand,secondRatio);
+    setRoation(minuteHand,minuteRatio)
+
+    
+}
+
+function setRoation(element, roationRatio){
+    element.style.setProperty('--rotation',roationRatio * 360);
+}
+
+setClock()
